6f7f225e1d13c9ebd855122608d1a0261b4ab7d9
Michalina Maria <michalina.dev@gmail.com>
2024-01-02 20:07:14 +0100

    type conversion

---
+++ misc/type_conversion.py
@@ -0,0 +1,14 @@
+def main():
+    round(123.45678, 3)
+    # zwraca zaokrąglenie do 3 cyfr czyli 123.457
+    round(56.78)
+    # zwraca 56, bo część po przecinku jest ucięta
+    int("56")
+    # zwraca integer 56
+    str(8)
+    # zwraca argument w stringu
+
+
+if __name__ == "__main__":
+    main()
+
