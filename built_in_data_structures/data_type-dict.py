5c2eb7d24da7700d11097fb4d74cca21e81f7f20
Michalina Maria <michalina.dev@gmail.com>
2024-01-04 20:01:06 +0100

    dict

--- built_in_data_structures/data_type-dict.py
+++ built_in_data_structures/data_type-dict.py
@@ -1,5 +1,30 @@
 def main():
-    pass
+
+    # tworzenie słownika
+    age = {"Marta": 38, "Misia": 37, "Gaston": 5, 999: "nie wiem"}
+
+    # Pobieranie wartości
+
+    # sposób 1. Wyrzuca KeyError jeśli klucza nie ma
+    number = age["Marta"]
+
+    # sposób 2. Zwraca None jeśli klucza nie ma
+    number = age.get("Marta")
+
+    # sposób 3. Zwraca wartość domyślną jeśli klucza nie ma (tu 0)
+    number = age.get("Marta", 0)
+
+    # dodawanie do słownika nowego klucza lub podmiana wartości dla istniejącego
+    age["Kasia"] = 39
+    age["Gaston"] = 999
+
+    # usuniecie ze słownika. Wyrzuca KeyError jeśli nie ma klucza.
+    # do testu czy jest takiklucz, można używac in set lub not in set
+    del age["Gaston"]
+
+
+
+
 
 
 if __name__ == "__main__":
