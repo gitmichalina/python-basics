4412b3ed7585e8525dc08a51cd13ae368d1ba7b8
Michalina Maria <michalina.dev@gmail.com>
2024-01-04 19:14:11 +0100

    tuple

--- built_in_data_structures/data_type-tuple.py
+++ built_in_data_structures/data_type-tuple.py
@@ -1,5 +1,36 @@
 def main():
-    pass
+
+    #można zapisywać bez nawiasów
+    a, b, c = 1, 2, 3
+
+    # pusta tuple
+    # tuple = ()
+
+    # tuple z jednym elementem ma przecinek inaczej będzie integerem
+    tuple = (3, )
+
+    # tworzenie tuple
+    x = (1, 2, 3)
+    x = 1, 2, 3
+
+    # przypisanie wartości liczbom przez tuple
+    a, b, c = x
+    a, b, c, = "xyz"
+
+    # pobranie wartości
+    y = x[1]
+
+    # pobranie fragmentu tuple
+    z = x[1:]
+
+    # zamiana wartości
+    a, b = b, a
+
+
+
+    # tuple jako typ wracany. wynik dzielenia i modulo tych dwóch liczb
+    tuple = divmod(20, 7)
+
 
 
 if __name__ == "__main__":
