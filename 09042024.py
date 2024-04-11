1d65b9888b3edeb4caf549b3ce80ad6730ad0f08
Michalina Maria <michalina.dev@gmail.com>
2024-04-11 21:13:01 +0200

    same practice

--- 09042024.py
+++ 09042024.py
@@ -3,3 +3,9 @@ print(id(a))
 b = a[:]
 print(b)
 print(id(b))
+
+eng2sp = {"one": "uno", "two": "dos", "three": "tres"}
+print(type(eng2sp.values()))
+print(eng2sp.values())
+vals = list(eng2sp.values())
+
